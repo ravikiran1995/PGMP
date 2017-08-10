9e09a4436dbbdbc23e8682892ac3af97e4b7bc58
rjravi1995 <rjravi.knr@gmail.com>
2017-08-11 00:29:50 +0530

    initial

--- app/src/main/java/com/example/ravikiran/pgmp/MainActivity.java
+++ app/src/main/java/com/example/ravikiran/pgmp/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.ravikiran.pgmp;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout= (DrawerLayout) findViewById(R.id.navigation_layout);
+        mToggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+if (mToggle.onOptionsItemSelected(item))
+{
+    return true;
+}
+return super.onOptionsItemSelected(item);
     }
 }
